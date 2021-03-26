65bc0ac5a83f495099743a0200442a60a4fb307c
bronemos <spieglb@gmail.com>
2021-03-26 20:10:23 +0100

    finish task 6

--- lab1/zadatak6.sh
+++ lab1/zadatak6.sh
@@ -10,3 +10,11 @@ if [ ! -d $1 ] || [ ! -d $2 ]; then
     exit 1
 fi
 
+files=$(echo "$(ls $1) $(ls $2)" | xargs -n1 | sort -u)
+
+for file in $files
+do
+    if [ $1$file -nt $2$file ]; then echo "$1$file --> $(echo $2 | sed -r 's/(.*)\//\1/')"
+    elif [ $2$file -nt $1$file ]; then echo "$2$file --> $(echo $1 | sed -r 's/(.*)\//\1/')"
+    fi
+done
